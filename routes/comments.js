e5d46ef5d585981f3c7452b538e1c3dbbcdb6d78
Matthew Miller <millerml352@gmail.com>
2020-10-10 10:13:24 -0700

    Comment EDIT and UPDATE routes and edit button on campground show page

--- routes/comments.js
+++ routes/comments.js
@@ -39,6 +39,30 @@ router.post("/", isLoggedIn, (req, res) => {
 	});
 });
 
+
+// Edit comment
+router.get("/:comment_id/edit", (req, res) => {
+	Comment.findById(req.params.comment_id, (err, foundComment) => {
+		if (err) {
+			console.log(err);
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+		}
+	});
+});
+
+// Update comment
+router.put("/:comment_id", (req, res) => {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+		if (err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	})
+});
+
 module.exports = router;
 
 // Middleware
