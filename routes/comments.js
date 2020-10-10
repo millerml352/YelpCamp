38dc2e4947ad4816dccf9d0e6ebd52fc116b02eb
Matthew Miller <millerml352@gmail.com>
2020-10-10 10:26:10 -0700

    Added comment destroy route and delete FORM on show page using method override

--- routes/comments.js
+++ routes/comments.js
@@ -63,6 +63,17 @@ router.put("/:comment_id", (req, res) => {
 	})
 });
 
+// Destroy comment
+router.delete("/:comment_id", (req, res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+		if (err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 module.exports = router;
 
 // Middleware
